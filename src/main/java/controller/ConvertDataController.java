c5d35530895f8046d23e0594b526730fa1138c8d
jhkim <jhkim@nextree.co.kr>
2018-09-11 17:38:44 +0900

    출력결과값 URL 변경

--- src/main/java/controller/ConvertDataController.java
+++ src/main/java/controller/ConvertDataController.java
@@ -27,7 +27,7 @@ public class ConvertDataController {
      * 입력값에 대해 규칙에 맟게 변환된 몫과 나머지 출력 기능
      * @return
      */
-    @RequestMapping(value = "/sort", method = RequestMethod.POST )
+    @RequestMapping(value = "/", method = RequestMethod.POST )
     public ModelAndView sort(@ModelAttribute("inputData") String inputData, @ModelAttribute("bindUnit") String bindUnit) {
         ModelAndView mv = new ModelAndView();
 
--- src/main/webapp/WEB-INF/jsp/sort.jsp
+++ src/main/webapp/WEB-INF/jsp/sort.jsp
@@ -7,7 +7,7 @@
 <title>입력값과 출력묶음단위에 대한 몫과 나머지 구하기</title>
 </head>
 <body>
-    <form action="/sort" method="post">
+    <form action="/" method="post">
         입력 : <textarea id="inputData" name="inputData">${inputData}</textarea> <br>
         출력묶음단위 : <input id="bindUnit" name="bindUnit" type="text"  value=${bindUnit}></input><br>
        <button type="submit">출력</button>
